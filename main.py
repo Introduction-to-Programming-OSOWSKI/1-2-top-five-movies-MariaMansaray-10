dd350c19fd684be274751403be7e7947082d2872
MariaMansaray10 <833868@mystma.org>
2022-09-16 14:28:03 -0500

    3

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def topFiveMovies(a,b,c,d,e):
+def topFiveMovies(a ,b ,c ,d ,e):
     return  a + b +c + d + e 
     topFiveMovies ("harry potter" , "spider man retur home" , "old" , "scary stories to tell in the dark" , "harry potter 2", )
